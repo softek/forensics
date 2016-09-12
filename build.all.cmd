4ad8a16fb9ae27a1a97e9e501cca088d41f4f5d7
Jeremy R. Sellars <jeremy.sellars@softekinc.com>
2016-09-12 15:05:28 -0500

    Vordsion generates a nice version code name

--- build.all.cmd
+++ build.all.cmd
@@ -1,4 +1,4 @@
 call build.modules.cmd
-del fileinfo.clj.dll fileinfo.exe regdiff.clj.dll regdiff.exe diceware.clj.dll diceware.exe
-Clojure.Compile.exe deps regdiff diceware fileinfo
+del fileinfo.clj.dll fileinfo.exe regdiff.clj.dll regdiff.exe diceware.clj.dll diceware.exe vordsion.clj.dll vordsion.exe
+Clojure.Compile.exe deps regdiff diceware fileinfo vordsion
 dir /b Clojure* |fileinfo --sha1-dice --ignore-system-path --full-path
