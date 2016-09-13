6083cfadcd31feaba7165fbd352b7c682579b701
Jeremy Sellars <jeremy.sellars@softekinc.com>
2016-09-13 15:54:25 -0500

    Renamed vordsion to codename by popular demand ;-)

--- build.all.cmd
+++ build.all.cmd
@@ -1,4 +1,4 @@
 call build.modules.cmd
-del fileinfo.clj.dll fileinfo.exe regdiff.clj.dll regdiff.exe diceware.clj.dll diceware.exe vordsion.clj.dll vordsion.exe
-Clojure.Compile.exe deps regdiff diceware fileinfo vordsion
+del fileinfo.clj.dll fileinfo.exe regdiff.clj.dll regdiff.exe diceware.clj.dll diceware.exe codename.clj.dll codename.exe
+Clojure.Compile.exe deps regdiff diceware fileinfo codename
 dir /b Clojure* |fileinfo --sha1-dice --ignore-system-path --full-path
