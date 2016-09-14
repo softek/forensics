bd01b24577e5cff1f9e5430de68ade8cd6c8eacc
Jeremy R. Sellars <jeremy.sellars@softekinc.com>
2016-09-14 18:39:12 -0500

    Add package build script

--- build.all.cmd
+++ build.all.cmd
@@ -2,4 +2,4 @@ call build.modules.cmd
 call pre-build.cmd .
 del fileinfo.clj.dll fileinfo.exe regdiff.clj.dll regdiff.exe diceware.clj.dll diceware.exe codename.clj.dll codename.exe
 Clojure.Compile.exe deps regdiff diceware fileinfo codename
-dir /b Clojure* |fileinfo --sha1-dice --ignore-system-path --full-path
+dir /b *dll *.exe |fileinfo --sha1 --ignore-system-path
