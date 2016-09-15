e42bc7b41b5d8eab970782dd77d59c9f7c3d96f3
Jeremy R. Sellars <jeremy.sellars@softekinc.com>
2016-09-14 19:21:33 -0500

    Fix archive zip contents.  Display Script Version

--- Archive.cmd
+++ Archive.cmd
@@ -1,10 +1,11 @@
 @ECHO OFF
 SETLOCAL
+set ArchiveVersion=Alpha
 set Status=.status.txt
 set binset=.binset.txt
 set archiveset=.archiveset.txt
 echo Forensics Archive %date% %time%>%status%
-echo.>>%status%
+echo.>>"%status%"
 
 call :Identify
 call :Zip
@@ -14,14 +15,17 @@ goto :EOF
   if exist "%binset%" del "%binset%"
   dir /b apptest.exe authview.exe crmtest.exe ddirdump.exe ejstest.exe enumlog.exe getlog.exe mon_ss.exe mqss_audit.exe msgview.exe qcpview.exe quemon.exe qxcr.exe scpview.exe setlog.exe srvconfig.exe srvmon.exe sysmon.exe tdbview.exe testdns.exe testsec.exe >> "%binset%"
   deps.exe scpview.exe >> "%binset%" 
-  type "%binset%"|find /i /v "logical.dll"|FileInfo --sha1|codename.exe>>%status%
+  type "%binset%"|find /i /v "logical.dll"|FileInfo --sha1|codename.exe>>"%status%"
+  ECHO.>>"%status%"
+  ECHO Archive Script Version: %ArchiveVersion%>>"%status%"
   goto :EOF
 
 :Zip
   type "%binset%">"%archiveset%"
   dir /b "%Status%">>"%archiveset%"
   call :SetArchive %cd%
-  type "%archiveset%"|FileInfo --ignore-system-path| zip -9 "%archive%" -@ -xi *.zip 
+  if exist "%archive%" del "%archive%"
+  type "%archiveset%"|FileInfo --ignore-system-path| zip -9 "%archive%" -@ -x *.zip 
   goto :EOF
 
 :SetArchive
