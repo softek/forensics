3711d4aaec8fd8573cbd7b5d9764f753b1ba6cb5
Jeremy R. Sellars <jeremy.sellars@softekinc.com>
2016-09-14 21:23:35 -0500

    ignore-SYSTEM-path for consistent hash. wrap dspmqver output in === lines

--- Archive.cmd
+++ Archive.cmd
@@ -16,16 +16,20 @@ goto :EOF
   if exist "%binset%" del "%binset%"
   dir /b apptest.exe authview.exe crmtest.exe ddirdump.exe ejstest.exe enumlog.exe getlog.exe mon_ss.exe mqss_audit.exe msgview.exe qcpview.exe quemon.exe qxcr.exe setlog.exe srvconfig.exe srvmon.exe sysmon.exe tdbview.exe testdns.exe testsec.exe >> "%binset%"
   deps.exe scpview.exe >> "%binset%" 
-  type "%binset%"|find /i /v "logical.dll"|FileInfo --sha1 --ignore-path|codename.exe>>"%status%"
+  type "%binset%"|find /i /v "logical.dll"|FileInfo --sha1 --ignore-system-path|codename.exe>>"%status%"
   ECHO.>>"%status%"
   ECHO Archive Script Version: %ArchiveVersion%>>"%status%"
   ECHO.>>"%status%"
   goto :EOF
 
 :MQ
+  ECHO.>>"%status%"
+  ECHO============================================================================>>"%status%"
   where dspmqver.exe >nul 2>nul
   if errorlevel 1        ECHO Can't find dspmqver.exe>>"%status%"
   if not errorlevel 1    dspmqver.exe -p 1 >>"%status%"
+  ECHO.>>"%status%"
+  ECHO============================================================================>>"%status%"
   ECHO.>>"%status%"
   goto :EOF
 
