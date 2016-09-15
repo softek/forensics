d92e7d6293f244e4aadd015b3f0703f2f299fe5c
Jeremy R. Sellars <jeremy.sellars@softekinc.com>
2016-09-14 19:15:00 -0500

    Fix Package-Forensics file list and printing

--- Package-Forensics.cmd
+++ Package-Forensics.cmd
@@ -3,13 +3,18 @@ SETLOCAL
 git pull
 set Status=.status.txt
 ECHO Forensics Package %date% %time%>%status%
+ECHO.>>%status%
 git describe --long --tags --dirty >>%status%
 git rev-parse --abbrev-ref HEAD >>%status%
-git log "--pretty=format:%%h %%ad %%s%%d [%%an]" --graph --date=short -n 10 >>%status%
+ECHO.>>%status%
+ECHO Recent Changes:>>%status%
+git log "--pretty=format:%%h %%ad %%s%%d [%%an]%%x0D" --graph --date=short -n 10 >>%status%
+ECHO.>>%status%
+ECHO.>>%status%
 call build.all.cmd
 dir /b *dll *.exe |fileinfo --sha1 --ignore-system-path >>%status%
 if exist Forensics.zip del Forensics.zip
-zip -9 Forensics.zip * -xi *.zip .gitignore .*set.txt
+zip -9 Forensics.zip * -x *.zip .gitignore .*set.txt
 ECHO ============================== Packaging Complete ============================
 type %status%
 dir Forensics.zip
