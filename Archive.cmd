4af91a167da0eaf37f27ba7db3b8c9f547f6b6c0
Jeremy Sellars <jeremy.sellars@softekinc.com>
2016-09-16 12:52:52 -0500

    Add registry export

--- Archive.cmd
+++ Archive.cmd
@@ -1,15 +1,19 @@
 @ECHO OFF
 SETLOCAL
 set ArchiveVersion=Alpha
-set Status=.status.txt
+set Status=.about.txt
 set binset=.binset.txt
 set archiveset=.archiveset.txt
+set RegFile=.cerner.reg
+set RegPath=%temp%\%RegFile%
+
 echo Forensics Archive %date% %time%>%status%
 echo.>>"%status%"
 
 call :Identify
 call :MQ
 call :Path
+call :Registry
 call :Zip
 
 goto :EOF
@@ -17,7 +21,7 @@ goto :EOF
 :Identify
   if exist "%binset%" del "%binset%"
   dir /b apptest.exe authview.exe crmtest.exe ddirdump.exe ejstest.exe enumlog.exe getlog.exe mon_ss.exe mqss_audit.exe msgview.exe qcpview.exe quemon.exe qxcr.exe setlog.exe srvconfig.exe srvmon.exe sysmon.exe tdbview.exe testdns.exe testsec.exe >> "%binset%" 2> nul
-  deps.exe scpview.exe >> "%binset%" 
+  deps.exe scpview.exe >> "%binset%"
   type "%binset%"|find /i /v "logical.dll"|FileInfo --sha1 --ignore-system-path|codename.exe>>"%status%"
   ECHO.>>"%status%"
   ECHO Archive Script Version: %ArchiveVersion%>>"%status%"
@@ -35,6 +39,15 @@ goto :EOF
   ECHO.>>"%status%"
   goto :EOF
 
+:Registry
+  ECHO.>>"%status%"
+  ECHO================================ Logicals (%RegFile%) ================================>>"%status%"
+  reg export "HKLM\Software\Wow6432Node\Cerner" "%RegPath%" /y
+  type "%RegPath%">>"%status%"
+  ECHO============================================================================>>"%status%"
+  ECHO.>>"%status%"
+  goto :EOF
+
 :Zip
   where 7z.exe >nul 2>nul
   if errorlevel 1 set path=%path%;c:\Program Files\7-Zip;c:\Program Files (x86)\7-Zip
@@ -45,11 +58,13 @@ goto :EOF
   call :SetArchive "%cd%"
   if exist "%archive%" del "%archive%"
   type "%archiveset%"|FileInfo --ignore-system-path>"%archiveset%2"
-  7z a "%archive%" @"%archiveset%2"
+  echo %RegPath%>>"%archiveset%2"
+  7z.exe a "%archive%" @"%archiveset%2"
 
   if exist "%binset%" del "%binset%"
   if exist "%archiveset%" del "%archiveset%"
   if exist "%archiveset%2" del "%archiveset%2"
+  if exist "%RegPath%" del "%RegPath%"
   goto :EOF
 
 :Path
